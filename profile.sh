7238a6f90f1a62bdfb747851ffe011cea8e2d87c
David Thole <david@thedarktrumpet.com>
2020-02-25 21:09:03 -0600

    Adding block for loading the creds file itself

--- profile.sh
+++ profile.sh
@@ -5,9 +5,15 @@
 #   Loads: .creds/sqldocker.txt which should contain the the args for the docker instance
 #          See the documentation in the github repo for more information about this.
 
+CREDSLOC="$HOME/.creds/dockersql"
 SCRIPTPATH=$0
 DIRLOC="$(dirname $SCRIPTPATH)"
 
 # Binaries
 export PATH="$DIRLOC/bin:$PATH"
 
+if test -f "$CREDSLOC"; then
+    source "$CREDSLOC"
+else
+    echo "Warning: $CREDSLOC does not exist.  Please review the readme documentation on how to setup this file, and use the sample.  The scripts in this repo will not perform as expected otherwise"
+fi
